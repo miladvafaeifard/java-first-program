f0ec5fbbb550dd161a7466780d3c43c639b9102f
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2020-11-23 18:26:31 +0100

    Fix add test

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -14,4 +14,12 @@ public class App
     public static int doubleTheNumber(int number) {
         return 2 * number;
     }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int number:numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
