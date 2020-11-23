ac34364d7fd745fb1fba094064c6ad878fe574d6
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2020-11-23 19:29:13 +0100

    Implement best loan rates class

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,38 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        var name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("Enter the loan term (in years)");
+        var loanTermInYears = scanner.nextInt();
+        var bestRate = getRates(loanTermInYears);
+        if (bestRate == 0.0f) {
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
+        } else {
+            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
+        }
+
+        scanner.close();
+    }
+
+    public static float getRates(int loanTermInYears) {
+        if(bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        }
+
+        return 0.0f;
+    }
+}
